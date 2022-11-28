84bdc27ce19136794cdd9eab3fdbee151f27fa53
liamasis <liamcalebob@gmail.com>
2022-11-28 23:29:18 +1100

    remove import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import React, { useState } from "react";
 import Modal from "../src/Modal";
 import styles from "../styles/Home.module.css";
